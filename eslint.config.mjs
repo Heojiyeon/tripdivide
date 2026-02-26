4d92b62b837e5e5d11eb803550b338955bec1565
Heo Ji Yeon <wldus7107@naver.com>
2026-02-26 23:08:35 +0900

    init: 프로젝트 초기 세팅

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+import unusedImports from "eslint-plugin-unused-imports";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  // Override default ignores of eslint-config-next.
+  eslintConfigPrettier,
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+  {
+    plugins: {
+      "unused-imports": unusedImports,
+    },
+    rules: {
+      "unused-imports/no-unused-imports": "error",
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+    },
+  },
+]);
+
+export default eslintConfig;
