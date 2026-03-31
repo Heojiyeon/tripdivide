c61cbe3a1062daa78bbf43a45d6a1d4f550e94af
Heo Ji Yeon <wldus7107@naver.com>
2026-03-31 21:07:39 +0900

    fix: 모달 새로고침 시 배경 슬롯 누락 에러 해결

---
+++ app/demo/[demoKey]/trips/[tripId]/settlement/page.tsx
@@ -0,0 +1,45 @@
+import Modal from "@/components/common/Modal";
+import { formatAmount } from "@/lib/format";
+import { ApiResponse, SettlementResponse } from "@/types/api";
+
+export default async function Settlement({
+  params,
+}: {
+  params: Promise<{ demoKey: string; tripId: string }>;
+}) {
+  const { demoKey, tripId } = await params;
+
+  const res = await fetch(`${process.env.API_URL}/api/demo/${demoKey}/trips/${tripId}/settlement`);
+
+  if (!res.ok) {
+    console.error("지출 결과 조회 실패");
+    return;
+  }
+
+  const { data } = (await res.json()) as ApiResponse<SettlementResponse>;
+  const { title, totalAmount, participants, transactions } = data;
+
+  return (
+    <Modal title={"📃 정산 결과서"}>
+      <div className="py-10 text-center">
+        <p>여행 제목 | {title}</p>
+        <p>총 지출 금액 | {formatAmount(totalAmount)}원</p>
+        <div>
+          여행 참여자 |&nbsp;
+          {participants.map((participant) => (
+            <span key={participant.id}>{participant.name} </span>
+          ))}
+        </div>
+      </div>
+      <div className="py-10 text-center">
+        정산 내역
+        {transactions.map((transaction, idx) => (
+          <p key={idx} className="font-semibold">
+            <span>{transaction.from}</span> ➡️&nbsp;
+            <span>{transaction.to}</span> | {formatAmount(transaction.amount)}원 송금
+          </p>
+        ))}
+      </div>
+    </Modal>
+  );
+}
