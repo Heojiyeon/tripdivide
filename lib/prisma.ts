87acda533051c3cd2135007b4e24ce13c68592be
Heo Ji Yeon <wldus7107@naver.com>
2026-04-06 20:08:00 +0900

    chore: 커넥션 풀링 및 커넥션 수 제한

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,13 +5,13 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL!, max: 1 });
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
     adapter,
-    log: ["query"],
+    log: process.env.NODE_ENV === "development" ? ["query"] : [],
   });
 
 if (process.env.NODE_ENV !== "production") {
